a549918e5b3ac99c008cd09b125bf65d62a033ad
etan-nestor <etannestor45@gmail.com>
2024-06-26 12:05:29 +0000

    commit06

--- src/components/Autres/BottomNav.jsx
+++ src/components/Autres/BottomNav.jsx
@@ -1,8 +1,8 @@
+import { BsFacebook } from "react-icons/bs"; 
 import { BsTelegram } from "react-icons/bs";
 import { FaInstagramSquare } from "react-icons/fa";
 import { BsDiscord } from "react-icons/bs";
 import { Link } from 'react-router-dom';
-import { CiFacebook } from "react-icons/ci";
 import React, { useState, useEffect } from 'react';
 import { Typewriter } from 'react-simple-typewriter';
 
@@ -21,7 +21,7 @@ const BottomNav = () => {
     }, [wordIndex, words]);
 
     return (
-        <div className="bg-green-300 py-4 dark:bg-gray-600 flex justify-between items-center">
+        <div className="bg-green-300 py-4 dark:bg-gray-600 flex justify-between items-center mt-[4rem]">
             <div className="flex flex-col md:flex-row items-center gap-5 ml-4 md:ml-[8em]">
                 <h5 className="border-b-4 border-b-slate-500 text-[18px] text-black font-semibold cursor-pointer hover:text-gray-800 dark:text-white dark:border-b-4 dark:border-b-green-500">
                     Nouveaux messages
@@ -31,7 +31,7 @@ const BottomNav = () => {
                 </h5>
             </div>
             <div className="absolute left-[50%] transform -translate-x-1/2 md:static md:transform-none md:left-auto">
-                <h1 className="text-2xl font-semibold dark:text-white">
+                <h1 className="text-xl font-semibold dark:text-white">
                     BAYLife {' '}
                     <span className={colors[wordIndex]}>
                         <Typewriter
@@ -47,17 +47,17 @@ const BottomNav = () => {
                 </h1>
             </div>
             <div className="mr-4 md:mr-[8em] flex justify-center items-center gap-5">
-                <Link>
-                    <CiFacebook />
+                <Link className="hover:transform hover:translate-y-1 transition duration-300">
+                    <BsFacebook  className="text-2xl dark:text-white text-black" />
                 </Link>
-                <Link>
-                    <BsDiscord />
+                <Link className="hover:transform hover:-translate-y-1 transition duration-300">
+                    <BsDiscord className="text-2xl dark:text-white text-black" />
                 </Link>
-                <Link>
-                    <FaInstagramSquare />
+                <Link className="hover:transform hover:translate-y-1 transition duration-300">
+                    <FaInstagramSquare className="text-2xl dark:text-white text-black" />
                 </Link>
-                <Link>
-                    <BsTelegram />
+                <Link className="hover:transform hover:-translate-y-1 transition duration-300">
+                    <BsTelegram className="text-2xl dark:text-white text-black" />
                 </Link>
             </div>
         </div>
--- src/components/Autres/Nav.jsx
+++ src/components/Autres/Nav.jsx
@@ -27,14 +27,17 @@ const Nav = () => {
     };
 
     return (
-        <div className="bg-green-400 dark:bg-gray-700 py-2">
+        <div className="bg-green-400 fixed w-full md:h-[4rem] h-[3rem] dark:bg-gray-700 py-2">
+
             <div className="flex justify-between items-center px-4 md:px-[8em]">
+                {/* menus */}
                 <div className="flex items-center gap-4">
-                    <div className="md:hidden">
-                        <button onClick={() => setIsMenuOpen(!isMenuOpen)}>
+                    <div className="md:hidden mr-2">
+                        <button className='px-2 py-1 bg-green-500 drop-shadow-md rounded-full' onClick={() => setIsMenuOpen(!isMenuOpen)}>
                             <BiMenu className="text-2xl text-white" />
                         </button>
                     </div>
+                    {/* items */}
                     <div className="hidden md:block">
                         <ul className="flex justify-center items-center gap-10">
                             {ListItems.map((item) => (
@@ -65,13 +68,17 @@ const Nav = () => {
                     </div>
                 </div>
 
-                <div className="flex justify-center items-center gap-5">
-                    <button className="hidden md:block cursor-pointer text-xl dark:bg-gray-700 dark:text-white dark:border-gray-100 dark:hover:border-green-400 font-semibold drop-shadow-lg text-black px-4 py-2 rounded-lg bg-green-400 border border-gray-800 hover:border-white hover:border-1 transition duration-300 ease-in-out">
+                {/* buttons */}
+                <div className="flex justify-center items-center gap-5 md:-mr-[7em] -mr-6">
+                    <button className="md:block cursor-pointer text-sm md:text-xl dark:bg-gray-700 dark:text-white dark:border-gray-100 dark:hover:border-green-400 font-semibold drop-shadow-lg text-black md:px-4 md:py-2 px-2 py-1 rounded-lg bg-green-400 border border-gray-800 hover:border-white hover:border-1 transition duration-300 ease-in-out">
                         Connexion
                     </button>
+                    <button className="hidden md:block cursor-pointer text-xl dark:bg-gray-700 dark:text-white dark:border-gray-100 dark:hover:border-green-400 font-semibold drop-shadow-lg text-black px-4 py-2 rounded-lg bg-green-400 border border-gray-800 hover:border-white hover:border-1 transition duration-300 ease-in-out">
+                        S'inscrire
+                    </button>
                     <div className="flex justify-center items-center gap-2 relative" ref={searchContainerRef} onBlur={handleBlur}>
-                        <div onClick={handleSearchIconClick} className="cursor-pointer bg-green-500 px-2 py-2 rounded-full drop-shadow-lg">
-                            <BiSearchAlt className="text-2xl text-white font-bold" />
+                        <div onClick={handleSearchIconClick} className="cursor-pointer bg-green-500 md:px-2 md:py-2 py-1 px-1 rounded-full md:drop-shadow-lg drop-shadow-sm">
+                            <BiSearchAlt className="md:text-2xl text-sm text-white md:font-bold text-semibold" />
                         </div>
                         <input className="hidden md:block bg-green-500 border w-[200px] py-2 rounded-lg text-white border-green-400 placeholder-gray-200 shadow-inner drop-shadow-lg outline-none text-xl font-semibold px-7" type="text" ref={searchInputRef} onFocus={() => setShowSearch(true)} placeholder="Rechercher......" />
                         {showSearch && <Search />}
@@ -82,28 +89,30 @@ const Nav = () => {
                 </div>
             </div>
 
+            {/* responsive menu items */}
+
             {isMenuOpen && (
-                <div className="md:hidden">
-                    <ul className="flex flex-col items-start gap-4 px-4 py-2">
+                <div className="md:hidden mt-2 bg-green-500 dark:bg-gray-800 w-[200px] h-[14rem] rounded-lg">
+                    <ul className="flex flex-col items-start justify-center pt-4 gap-4 px-4 py-1">
                         {ListItems.map((item) => (
-                            <li className="cursor-pointer text-xl dark:bg-gray-700 dark:text-white dark:border-gray-700 dark:hover:border-green-400 font-semibold drop-shadow-lg text-black px-4 py-2 rounded-lg bg-green-400 border border-green-400 hover:border-white hover:border-1 transition duration-300 ease-in-out" key={item.id}>
+                            <li className="cursor-pointer w-full text-sm dark:text-white  dark:hover:border-green-400 font-semibold text-black px-2 py-1 rounded-md  border border-transparent hover:border-white hover:border-1 transition duration-300 ease-in-out" key={item.id}>
                                 <Link to={item.link}>{item.name}</Link>
                             </li>
                         ))}
-                        <li className="text-xl font-semibold dark:bg-gray-700 dark:text-white dark:border-gray-700 dark:hover:border-green-400 text px-4 py-1 rounded-lg drop-shadow-lg bg-green-400 group relative cursor-pointer border border-green-400 hover:border-white hover:border-1 transition duration-300 ease-in-out">
+                        <li className="cursor-pointer w-full text-sm dark:text-white  dark:hover:border-green-400 font-semibold text-black px-2 py-1 rounded-md  border border-transparent hover:border-white hover:border-1 transition duration-300 ease-in-out group relative  border-green-400 hover:border-1">
                             <Link to="/membres" className="flex items-center gap-[5px] py-1">
                                 Membres
                                 <span>
                                     <FaCaretDown className="transition-all duration-200 group-hover:rotate-180" />
                                 </span>
                             </Link>
-                            <div className="absolute z-[9999] hidden group-hover:block w-[400px] rounded-md bg-green-200 p-2 text-black dark:text-white dark:bg-gray-600 dark:hover:text-green-990">
+                            <div className="absolute z-[9999] hidden group-hover:block w-[300px] rounded-md bg-green-200 p-2 text-black dark:text-white dark:bg-gray-600 dark:hover:text-green-990">
                                 <ul>
                                     {DropDownLinks.map((data) => (
                                         <li key={data.id}>
-                                            <a className="inline-block w-full hover:bg-green-400 rounded-md p-2 dark:hover:bg-black hover:drop-shadow-lg transition duration-300 ease-in-out dark:hover:text-white" href={data.link}>
+                                            <Link className="inline-block w-full hover:bg-green-400 rounded-md p-2 dark:hover:bg-black hover:drop-shadow-lg transition duration-300 ease-in-out dark:hover:text-white" to={data.link}>
                                                 {data.name}
-                                            </a>
+                                            </Link>
                                         </li>
                                     ))}
                                 </ul>
@@ -112,6 +121,7 @@ const Nav = () => {
                     </ul>
                 </div>
             )}
+
             <UserGuide />
         </div>
     );
